da6acd9be580a790061b5f3b10c35e7bd51b5e62
Luis Guilherme(Shadow) <senaluisgf@gmail.com>
2021-06-21 14:56:42 -0400

    remove: unecessary files

--- index.js
+++
@@ -1,4 +0,0 @@
-function enviarEmail(nome, email) {
-    console.log("seu nome \u00E9 " + nome + " e seu email \u00E9 " + email);
-}
-enviarEmail('Dani', 'dani@gmail.com');
--- index.ts
+++
@@ -1,5 +0,0 @@
-function enviarEmail(nome: string, email: string){
-    console.log(`seu nome é ${nome} e seu email é ${email}`)
-}
-
-enviarEmail('Dani', 'dani@gmail.com')
