59388f447d650d93261c2603adacbb8a7bbe0185
Luis Guilherme(Shadow) <senaluisgf@gmail.com>
2021-06-21 15:23:54 -0400

    add: two new routes (get and post)

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,7 @@ import express from 'express'
 
 const app = express()
 
-app.listen(3000, () => console.log('server is running NLW'))
+app.listen(3000, () => console.log('server is running'))
+
+app.get('/teste', (req, res) => { return res.send('Primeira rota da aplicação')})
+app.post('/teste', (req, res) => { return res.send('Segunda rota da aplicação')})
