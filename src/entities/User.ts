1dfd84b07dbb0f7fbc87ac69a369baa9d92ad535
Luis Guilherme(Shadow) <sena.luisgf@gmail.com>
2021-06-24 16:03:57 -0400

    add: creating user repository

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,6 +1,26 @@
-import {Entity} from "typeorm";
+import {Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn} from "typeorm";
+import { v4 as uuid } from 'uuid'
 
-@Entity()
+@Entity('users')
 export class User {
+    @PrimaryColumn()
+    readonly id: string
 
+    @Column()
+    name: string
+    @Column()
+    email: string
+    @Column()
+    admin: boolean
+
+    @CreateDateColumn()
+    created_at: Date
+    @UpdateDateColumn()
+    updated_at: Date
+
+    constructor(){
+        if (!this.id){
+            this.id = uuid()
+        }
+    }
 }
